a388c5c329a21d1c43a112f9738a132ec439607f
matt77hias <matthias.moulin@gmail.com>
2014-04-15 10:39:13 +0200

    Docu added

--- src/vis1.py
+++ src/vis1.py
@@ -14,6 +14,12 @@ import loader as l
 import configuration as c
 
 def extract_landmarks(X, nr_trainingSample=1):
+    '''
+    Extract the landmark x and y coordinates for the given training sample.
+    @param X:                   the training samples
+    @param nr_trainingSample:   the training sample to select
+    @return The x and y coordinates of the given training sample.
+    '''
     nb = c.get_nb_landmarks()
     xCoords = np.zeros(nb)
     yCoords = np.zeros(nb)
@@ -24,6 +30,11 @@ def extract_landmarks(X, nr_trainingSample=1):
     return (xCoords, yCoords)
                   
 def plot_landmarks(X, nr_trainingSample=1):
+    '''
+    Plots the landmarks corresponding to the given training sample.
+    @param X:                   the training samples
+    @param nr_trainingSample:   the training sample to select
+    '''
     xCoords, yCoords = extract_landmarks(X, nr_trainingSample)
     # x coordinates , y coordinates
     pylab.plot(xCoords, yCoords, '-r')
@@ -32,6 +43,13 @@ def plot_landmarks(X, nr_trainingSample=1):
     pylab.show()
     
 def display_landmarks(X, nr_trainingSample=1, color=np.array([0, 0, 255])):
+    '''
+    Displays the landmarks corresponding to the given training sample
+    on the corresponding radiograph.
+    @param X:                   the training samples
+    @param nr_trainingSample:   the training sample to select
+    @param color:               the color used for displaying
+    '''
     s = "/"
     if (nr_trainingSample < 10):
         s = "/0"
@@ -43,6 +61,7 @@ def display_landmarks(X, nr_trainingSample=1, color=np.array([0, 0, 255])):
         #y coordinate , x coordinate
         img[yCoords[i], xCoords[i], :] = color
     #cv2.imshow("test", img)
+    #writing instead of showing, because the displayed image is too large
     cv2.imwrite("test.tif", img)
       
 if __name__ == '__main__':
