522c2e7b5508c5dfd31129b374b9bfff551796ec
matt77hias <matthias.moulin@gmail.com>
2014-04-15 10:34:27 +0200

    Cleanup + docu added

--- src/math_utils.py
+++ src/math_utils.py
@@ -11,35 +11,66 @@ import numpy as np
 def normalize_vector(v):
     '''
     Normalize the given vector.
+    @param v:           the vector to normalize.
+    @return The normalized vector.
     '''
     norm=np.linalg.norm(v)
     if norm==0: 
        return v
     return v/norm
     
-def rotate(v, s=1, theta=0):
-    sc = s*math.cos(theta)
-    ss = s*math.sin(theta)
-    r = np.zeros(v.shape)
-    for i in range(v.shape[0] / 2):
-        x = v[(2*i)]
-        y = v[(2*i+1)]
-        r[(2*i)] = sc*x-ss*y
-        r[(2*i+1)] = ss*x+sc*y
-    return r
-    
 def align_with(x1, x2):
+    '''
+    Aligns x1 with x2
+    @pre    x1 and x2 are centred on the origin
+    @pre    The landmark coordinates are stored as successive xi, yi, xj, yj, ...
+    @param x1           the vector to align
+    @param x2           the vector to align with
+    @return The aligned vector for x1
+    '''
     s, theta = align_with_params(x1, x2)
-    return rotate(x2, s, theta) 
+    return align(x2, s, theta) 
     
 def align_with_params(x1, x2):
+    '''
+    Returns the transformation parameters (s, theta) used for scaling
+    x1 with s and rotating x1 with theta in order to align the transformed
+    x1 with x2. This method tries to minimize |s.R(theta).x1-x2|.
+    @pre    x1 and x2 are centred on the origin
+    @pre    The landmark coordinates are stored as successive xi, yi, xj, yj, ...
+    @param x1           the vector to align
+    @param x2           the vector to align with
+    @return The transformation parameters (s, theta) for aligning x1 with x2.
+    '''
     n = pow(np.linalg.norm(x1), 2)
     a = np.dot(x1, x2) / n
     b = 0
     for i in range(x1.shape[0] / 2):
+        # landmark coordinates stored as successive xi, yi, xj, yj
         b += x1[(2*i)]*x2[(2*i+1)] - x1[(2*i+1)]*x2[(2*i)]
     b /= n
     
     s = math.sqrt(a*a+b*b)
     theta = math.atan(b/a)
     return (s, theta)
+    
+def align(v, s=1, theta=0):
+    '''
+    Aligns x1 with scaling parameter s and rotation parameter theta.
+    @pre    v is centred on the origin
+    @pre    The landmark coordinates are stored as successive xi, yi, xj, yj, ...
+    @param v:           the vector to align
+    @param s:           the scaling parameter
+    @param theta:       the rotation parameter
+    @return The aligned vector for v
+    '''
+    sc = s*math.cos(theta)
+    ss = s*math.sin(theta)
+    r = np.zeros(v.shape)
+    for i in range(v.shape[0] / 2):
+        # landmark coordinates stored as successive xi, yi, xj, yj
+        x = v[(2*i)]
+        y = v[(2*i+1)]
+        r[(2*i)] = sc*x-ss*y
+        r[(2*i+1)] = ss*x+sc*y
+    return r
