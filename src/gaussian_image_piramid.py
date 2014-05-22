dfe0027c34c8423dd4c55a7f8d9a354bbf48e192
MilanSamyn <milansamyn@gmail.com>
2014-05-22 12:10:06 +0200

    tussentijdse commit
    Nog niet klaar met het algoritme zelf, momenteel ook veel duplicate code
    enzo

--- src/gaussian_image_piramid.py
+++ src/gaussian_image_piramid.py
@@ -9,11 +9,24 @@ Used in the multi-resolution search algorithm.
 import cv2
 import configuration as c
 
-lmax = 3 #Coarsest level of gaussian pyramid (depends on the size of the object in the image)
+lmax = 1                        #Coarsest level of gaussian pyramid (depends on the size of the object in the image)
+method='SCD'                    #The method used for preproccesing.
 
 def form_pyramids():
     '''
-    Form gaussian image pyramids of all the test samples.
+    Form gaussian image pyramids of all the training and test samples.
+    '''
+    
+    for i in c.get_trainingSamples_range():
+        test_image = cv2.imread(c.get_fname_vis_pre(i, method))
+        for j in range(lmax+1):
+            if j == 0:
+                fname = c.get_fname_pyramids(i, j) 
+                cv2.imwrite(fname, test_image)
+            else:
+                test_image = cv2.pyrDown(test_image)
+                fname = c.get_fname_pyramids(i, j) 
+                cv2.imwrite(fname, test_image)
     '''
     for i in c.get_testSamples_range():
         test_image = cv2.imread(c.get_fname_test_radiograph(i))
@@ -25,6 +38,7 @@ def form_pyramids():
                 test_image = cv2.pyrDown(test_image)
                 fname = c.get_fname_pyramids(i, j) 
                 cv2.imwrite(fname, test_image)
+    '''
 
 if __name__ == '__main__':
     form_pyramids()
