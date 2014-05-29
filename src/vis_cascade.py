c975cf3bff22fe11d1e7d9a2589a9b1cd8a3005d
matt77hias <matthias.moulin@gmail.com>
2014-05-29 20:21:43 +0200

    Terug naar 13-13 samples

--- src/vis_cascade.py
+++ src/vis_cascade.py
@@ -2,18 +2,21 @@ import cv2
 import configuration as c
 
 def whaha():
-    cascade = cv2.CascadeClassifier("CV/data/Training/training/cascadesSCD1-u/cascade.xml")
-    fname = c.get_fname_vis_pre(1, 'SCD')
-    img = cv2.imread(fname)
-    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=1, minSize=(10, 10))
-    #result is an array of x coordinate, y coordinate, weight, height for each rectangle
-    rects[:,2:] += rects[:,:2]
-    #result is an array of x coordinate, y coordinate, x + weight, y + height for each rectangle (opposite corners)
     
-    for i in range(rects.shape[0]):
-        cv2.rectangle(img, (rects[i,0], rects[i,1]), (rects[i,2], rects[i,3]), (0, 255, 0), 2)
-        cv2.imshow('img', img)
-        cv2.waitKey(0)
+    for m in ['l', 'u']:
+        for i in c.get_trainingSamples_range():
+            cascade = cv2.CascadeClassifier("CV/data/Training/training/cascadesSCD" + str(i) + '-' + m + "/cascade.xml")
+            fname = c.get_fname_vis_pre(i, 'SCD')
+            img = cv2.imread(fname)
+            rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=1, minSize=(300, 150))
+            #result is an array of x coordinate, y coordinate, weight, height for each rectangle
+            rects[:,2:] += rects[:,:2]
+            #result is an array of x coordinate, y coordinate, x + weight, y + height for each rectangle (opposite corners)
+        
+            for r in range(rects.shape[0]):
+                cv2.rectangle(img, (rects[r,0], rects[r,1]), (rects[r,2], rects[r,3]), (0, 255, 0), 2)
+            fname = 'test' + str(i) + '-' + m + '.png'
+            cv2.imwrite(fname, img)
     
 if __name__ == '__main__':
     whaha()
