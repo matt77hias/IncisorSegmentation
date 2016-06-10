b4060cf5ef2a02b2a614d8ad95a95909dc96d7bb
Matthias <matthias.moulin@gmail.com>
2016-06-10 18:48:42 +0200

    Temp

--- src/principal_component_analysis.py
+++ src/principal_component_analysis.py
@@ -108,7 +108,7 @@ def pca_raw(X):
     
     #About the negative eigenvalues, it is just a matter of eig(h).
     #As eigenvalues shows the variance in a direction, we care about absolute
-    #value but if we change a sign, we also have to change the "direcction" (eigenvector).
+    #value but if we change a sign, we also have to change the "direction" (eigenvector).
     #You can make this multiplying negative eigenvalues and their corresponding eigenvectors with -1.0
     s = np.where(eigenvalues < 0)
     eigenvalues[s] = eigenvalues[s] * -1.0
